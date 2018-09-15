ab069a4743adcd7ddefb036a421bdc20a2b0003e
yagisuke <yagisuke.0209@gmail.com>
2018-09-15 11:26:49 +0900

    lesson3

--- app/app/MemoTableViewController.swift
+++ app/app/MemoTableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class MemoTableViewController: UITableViewController {
 
+    var memos =  ["one", "tow", "thuree"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -29,23 +31,22 @@ class MemoTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.memos.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MemoTableViewCell", for: indexPath)
 
         // Configure the cell...
+        cell.textLabel?.text = self.memos[indexPath.row]
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
